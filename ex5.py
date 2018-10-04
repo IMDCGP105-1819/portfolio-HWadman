53e1a2e336b7c1959c0499262ea27104a75db8f4
Harry Wadman <harry.wadman4@gmail.com>
2018-10-04 11:32:16 +0100

    ex5 Added

---
+++ ex5.py
@@ -0,0 +1,6 @@
+for x in range(99, 0, -1):
+    print(f"{x} bottles of beer on the wall, {x} bottles of beer.")
+    print(f"Take one down, pass it around, {x-1} bottles of beer on the wall...")
+
+print("No more bottles of beer on the wall, no more bottles of beer.")
+print("Go to the store and buy some more, 99 bottles of beer on the wall...")
