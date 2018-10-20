c4f86fcc44ef8c80b964b27bb2a9a26e836729b3
Harry Wadman <harry.wadman4@gmail.com>
2018-10-20 10:30:46 +0100

    Added guess counter to ex11

--- ex11.py
+++ ex11.py
@@ -4,12 +4,14 @@ high_num = 50
 low_num = 0
 number_to_guess = randint(low_num,high_num)
 number_guessed = -9999999
+number_of_guesses = 0
 
 while number_to_guess != number_guessed:
     number_guessed = int(input(f"Guess a number between {low_num} and {high_num}: "))
+    number_of_guesses += 1
 
     if number_guessed == number_to_guess:
-        print(f"Well done! The number was {number_to_guess}")
+        print(f"Well done! The number was {number_to_guess} it took you {number_of_guesses} guesses to reach that number")
     elif number_guessed > number_to_guess:
         print("Your number is higher than the number you need to guess")
     else:
